ea70adddc081ba312a7f738c1f1f883e5f0b2ff0
Freddie Miguel <th3trickstercard@gmail.com>
2026-04-17 22:28:08 -0400

    commit final

--- ch09_ex1_CustomerManager/src/CustomerDB.java
+++ ch09_ex1_CustomerManager/src/CustomerDB.java
@@ -1,48 +1,49 @@
+import java.io.*;
 import java.util.ArrayList;
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public final class CustomerDB {
 
-    private static final String FILENAME = "customers.txt";
-    private static final String COL_SEP = "\t";    
+    private static final String FILENAME = "Excercise-9-1/ch09_ex1_CustomerManager/src/customers.txt";
+    private static final String COL_SEP = "\t";
 
     public static ArrayList<Customer> getAll() {
         var customers = new ArrayList<Customer>();
-        
-        try (BufferedReader in = new BufferedReader(new FileReader(FILENAME))) {
+
+        File file = new File(FILENAME);
+        System.out.println("Looking for: " + file.getAbsolutePath());
+        System.out.println("Exists? " + file.exists());
+
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
             String line;
+
             while ((line = in.readLine()) != null) {
                 String[] columns = line.split(COL_SEP);
-                
+
                 if (columns.length == 3) {
                     String firstName = columns[0];
                     String lastName = columns[1];
                     String email = columns[2];
-                    
-                    Customer c = new Customer(firstName, lastName, email);
-                    customers.add(c);
+
+                    customers.add(new Customer(firstName, lastName, email));
                 }
             }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File not found -> " + FILENAME);
         } catch (IOException e) {
-            System.out.println("Error reading from " + FILENAME + ": " + e.getMessage());
+            System.out.println("Error reading file: " + e.getMessage());
         }
-        
+
         return customers;
     }
 
     public static void saveAll(ArrayList<Customer> customers) {
-        try (PrintWriter out = new PrintWriter(new FileWriter(FILENAME))) {
+        File file = new File(FILENAME);
+
+        try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(file)))) {
             for (Customer c : customers) {
-                out.print(c.getFirstName());
-                out.print(COL_SEP);
-                out.print(c.getLastName());
-                out.print(COL_SEP);
-                out.print(c.getEmail());
-                out.println();
+                out.println(c.getFirstName() + COL_SEP
+                        + c.getLastName() + COL_SEP
+                        + c.getEmail());
             }
         } catch (IOException e) {
             System.out.println(e);
